eb1c59c4ea8d84b35e2572d1a0e370267761350b
Sanket Tambe <tambesanket49.com>
2025-05-08 15:29:26 +0530

    update the code

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "region" {
+  default = "us-east-1"
+}
+
 variable "ami_id" {
   default = "ami-0f88e80871fd81e91"
 }
@@ -5,4 +9,6 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "S3 bucket to allow access from EC2"
 }
+
+
 
